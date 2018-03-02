de7038e19b5ef693ddd19c9c3975285407e51735
Nick Ashley <nick@shley.us>
2018-03-02 13:04:14 -0500

    Update dependency installation

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 # install client dependencies
-cd ~/build/BandwidthExamples/context-call/client
+cd ~/build/BandwidthExamples/context-call/client/
 npm install
 
 # install server dependencies
-cd ~/build/BandwidthExamples/context-call/server
-npm install node-bandwidth
+cd ~/build/BandwidthExamples/context-call/server/text/
+npm install
+
+cd ~/build/BandwidthExamples/context-call/server/call/
+npm install
 
 # restore initial working directory
 cd ~/build/BandwidthExamples/context-call/
