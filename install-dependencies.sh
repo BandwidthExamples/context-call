b86332bd60bbb448fece9ec77444062476bcfc7a
Nick Ashley <nick@shley.us>
2018-03-02 20:09:46 -0500

    use before_deploy

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -10,13 +10,8 @@ npm install
 cd ${TRAVIS_BUILD_DIR}/server/call/
 npm install
 
-cd ${TRAVIS_BUILD_DIR}/server/local_dependencies/aws-api-gateway-return
-npm install --production
-
-cd ${TRAVIS_BUILD_DIR}/server/local_dependencies/simple-bandwidth-api
-npm install --production
-
 # restore initial working directory
+# TODO necessary?
 cd ${TRAVIS_BUILD_DIR}/
 
 # pip install awscli --upgrade --user
