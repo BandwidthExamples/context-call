da43b3da4b0494255d53462abad74de6fcb3b8f7
Nick Ashley <nick@shley.us>
2018-03-02 19:32:52 -0500

    Install dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -10,7 +10,11 @@ npm install
 cd ${TRAVIS_BUILD_DIR}/server/call/
 npm install
 
-cd ${TRAVIS_BUILD_DIR}/server/local_dependencies/
+cd ${TRAVIS_BUILD_DIR}/server/local_dependencies/aws-api-gateway-return
+npm install --production
+
+cd ${TRAVIS_BUILD_DIR}/server/local_dependencies/simple-bandwidth-api
+npm install --production
 
 # restore initial working directory
 cd ${TRAVIS_BUILD_DIR}/
