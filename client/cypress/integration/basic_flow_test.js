0a3fe72c48c6984f2f9fe94498a9ec8f5b3c0e8d
Anthony Burns <amburns@ncsu.edu>
2018-03-13 14:27:02 -0400

    Made a fix the flow test

--- client/cypress/integration/basic_flow_test.js
+++ client/cypress/integration/basic_flow_test.js
@@ -1,6 +1,7 @@
 describe('Basic Flow Test', function(){
     it('test1', function(){
         cy.visit('http://call-me-maybe.s3-website-us-west-2.amazonaws.com/')
-        cy.contains
+        cy.get('#company-number')
+
     })
 })
