08465d2209175c25628adb71192225ea9dfd1e6e
Anthony Burns <amburns@ncsu.edu>
2018-03-14 11:11:50 -0400

    Finished the flow test, no validation yet

--- client/cypress/integration/basic_flow_test.js
+++ client/cypress/integration/basic_flow_test.js
@@ -2,6 +2,13 @@ describe('Basic Flow Test', function(){
     it('test1', function(){
         cy.visit('http://call-me-maybe.s3-website-us-west-2.amazonaws.com/')
         cy.get('#company-number')
-
+            .type('+18186971390')
+        cy.get('#customer-number')
+            .type('+17023429757')
+        cy.get('#text-message')
+            .type('here\'s my number so call me maybe.')
+        cy.get(':nth-child(4) > :nth-child(2) > .FlowItemContent-hJlKhy > .InputStyles-hMJoFY')
+            .type('password')
+        cy.get('#submit-button').click()
     })
 })
