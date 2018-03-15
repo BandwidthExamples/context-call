1abef8980985fd88ce0b6eb4ac22d7b8e62070bf
Nick Ashley <nick@shley.us>
2018-03-15 15:44:55 -0400

    Update call lambda to call numbers
    Untested; doesn't bridge yet

--- server/call/index.js
+++ server/call/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function callNumber(companyNumber, customerNumber, callback) {
+function callNumber(number, tag, callback) {
 	// customerNumber: 10 digit number to text
 	// companyNumber: the number to call once the appropriate wait has elapsed
 	// callback: the callback function to return to
@@ -8,12 +8,17 @@ function callNumber(companyNumber, customerNumber, callback) {
 
 	const postData = JSON.stringify({
 		from: process.env.PHONE_NUMBER,
-		to: customerNumber,
+		to: number,
 		callbackUrl: process.env.CALLBACK_WAIT_URL, // the URL of our API endpoint that will handle waiting and then calling
-		tag: JSON.stringify({'companyNumber': companyNumber, 'customerNumber': customerNumber}) // send the number of seconds to wait until calling as well as both numbers to call
+		tag: tag
 	});
 
 	bandwidthAPI.post('calls', postData, callback);
+}
+
+function bridgeCalls(idOne, idTwo, callback) {
+	const bandwidthAPI = require('simple-bandwidth-api');
+	callback("not defined");
 }
 
 exports.handler = (event, context, callback) => {
@@ -23,16 +28,39 @@ exports.handler = (event, context, callback) => {
 	context.callbackWaitsForEmptyEventLoop = false;
 
 	let body = JSON.parse(event.body);
-	if(!body.secret || body.secret != process.env.SECRET) {
+	switch(body.deliveryState) {
+		case 'waiting':
+			// text message was sent but no delivery report yet
+			callback(null, httpResponse.create(200, "")); // acknowledge the callback
+			break;
+		case 'delivered':
+			// continue running
+			// TODO move subsequent code to this block
+			break;
+	}
+	let tag = JSON.parse(body.tag); // our state, defined by the previous lambda call
+	if(!tag.secret || tag.secret != process.env.SECRET) {
 		callback(null, httpResponse.create(401, "secret was not specified or is invalid"));
 	}
 
-	switch(body.request) {
+	switch(tag.request) {
 		case 'call':
-			callNumber(body.companyNumber, body.customerNumber, callback);
+			// we'll set our number and the new tag depending on what step we're on
+			let number = "";
+			let new_tag = {};
+			if(tag.companyNumber) {
+				new_tag.request = 'call';
+				number = tag.companyNumber;
+				new_tag.customerNumber = tag.customerNumber;
+			} else if (tag.companyNumber) {
+				new_tag.request = 'bridgeCalls';
+				number = tag.companyNumber;
+				new_tag.customerCallId = body.callId;
+			}
+			callNumber(number, new_tag, callback);
 			break;
 		case 'bridgeCalls':
-			bridgeCalls(body.companyCallID, body.customerCallID, callback);
+			bridgeCalls(body.callId, tag.customerCallId, callback);
 			break;
 	}
 };
