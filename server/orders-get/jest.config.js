3e87c5f3715f0e6950b90668087819c14a6c87ae
Nick Ashley <nick@shley.us>
2018-04-10 14:58:58 -0400

    Combine the two order lambdas into one

--- server/orders-get/jest.config.js
+++
@@ -1,6 +0,0 @@
-// jest.config.js
-module.exports = {
-  verbose: true,
-  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)(spec|test).js?(x)'],
-  testPathIgnorePatterns: ['/node_modules/']
-};
---
+++ server/orders/index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const httpResponse = require('aws-api-gateway-return');
+AWS.config.update({region: 'us-west-2'});
+
+function getOrders(callback) {
+	// let db = new AWS.DynamoDB({region: 'us-west-2', apiVersion: '2012-08-10'});
+	let docClient = new AWS.DynamoDB.DocumentClient();
+
+	let params = {
+		TableName: 'orders'
+	};
+
+	docClient.scan(params, (err, data) => {
+		if(err) {
+			console.error('Unable to scan the table. Error: ',
+				JSON.stringify(err, null, 2));
+		} else {
+			console.log('Scan succeeded.');
+			if(typeof data.LastEvaluatedKey !== 'undefined') {
+				console.log('Not all data scanned.');
+			}
+			callback(null, httpResponse.create(200, data.Items));
+		}
+	});
+}
+
+function addOrder(orderId, name, phoneNumber, eta, callback) {
+	// let db = new AWS.DynamoDB({region: 'us-west-2', apiVersion: '2012-08-10'});
+	let docClient = new AWS.DynamoDB.DocumentClient();
+
+	let params = {
+		TableName: 'orders',
+		Item: {
+			'orderId': orderId,
+			'name': name,
+			'phoneNumber': phoneNumber,
+			'eta': eta
+		}
+	};
+
+	docClient.put(params, (err, data) => {
+		if(err) {
+			console.error('Unable to add to the table. Error: ',
+				JSON.stringify(err, null, 2));
+		} else {
+			console.log('Added to the table.');
+			callback(null, httpResponse.create(200, 'Added: ' + JSON.stringify(data)));
+		}
+	});
+}
+
+exports.handler = (event, context, callback) => {
+	context.callbackWaitsForEmptyEventLoop = false;
+
+	if(!event['queryStringParameters']) {
+		callback(null, httpResponse.create(401, 'invalid/unspecified query parameters'));
+	}
+	const secret = event['queryStringParameters']['secret'];
+	if(!secret || secret !== process.env.SECRET) {
+		callback(null, httpResponse.create(401, 'invalid/unspecified secret'));
+	}
+
+	switch(event['httpMethod']) {
+		case 'GET':
+			getOrders(callback);
+			break;
+		case 'POST':
+			addOrder(
+				event['queryStringParameters']['orderId'],
+				event['queryStringParameters']['name'],
+				event['queryStringParameters']['phoneNumber'],
+				event['queryStringParameters']['eta'],
+				callback
+			);
+			break;
+	}
+};
