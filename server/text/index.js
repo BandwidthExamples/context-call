d137595ae3c703692c305741f6ea5990802e8c11
Nick Ashley <nick@shley.us>
2018-05-07 19:15:13 -0400

    Add debug to step function execution call

--- server/text/index.js
+++ server/text/index.js
@@ -58,6 +58,7 @@ exports.handler = (event, context, callback) => {
 					input: JSON.stringify({'body':tag}),
 					name: crypto.createHash('md5').update(JSON.stringify(tag)).digest("hex") // we now have idempotent executions // TODO ensure this occurs before the text is sent or decide to get rid of this line
 				};
+				console.log("Starting step function...");
 				stepfunctions.startExecution(params, function(err, data) {
 					if (err)	callback(null, httpResponse.create(500, "Internal Server Error (" + err + "):\n" + err.stack)); // an error occurred
 					else		callback(null, httpResponse.create(200, "okay")); // successful response
